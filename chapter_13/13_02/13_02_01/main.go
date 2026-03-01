99a90d81ce76483e02ca5402215c13520e6d2fa5
Asanbek09 <samudinua@gmail.com>
2026-03-01 19:54:29 +0600

    Структуры, теги и аннотации

---
+++ chapter_13/13_02/13_02_01/main.go
@@ -0,0 +1,24 @@
+// Простая JSON-структура
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// type Name struct {
+// 	First string `json:"firstName"`
+// 	Last string `json:"lastName"`
+// }
+
+type Name struct {
+	First string `json:"firstName" xml:"FirstName"`
+	Last string `json:"lastName,omitempty"`
+	// Other string `not,even.a=tag`
+}
+
+func main() {
+	n := &Name{"Armor", "King"}
+	data, _ := json.Marshal(n)
+	fmt.Printf("%s\n", data)
+}
---
+++ chapter_13/13_02/13_02_03/example.go
@@ -0,0 +1,23 @@
+package main
+
+// Простая структура Processes
+// type Processes struct {
+// 	Total int
+// 	Running int
+// 	Sleeping int
+// 	Threads int
+// 	Load float32
+// }
+
+// Структура Processes2 с аннотациями
+// type Processes2 struct {
+// 	Total int `json:"total"`
+// 	Running int `json:"running"`
+// 	Sleeping int `json:"sleeping"`
+// 	Threads int `json:"threads"`
+// 	Load float32 `json:"load"`
+// }
+
+// Шаблоны методов Marshal и Unmarshal
+// func Marshal(v interface{}) ([]byte, error) {return nil, nil}
+// func Unmarshal(data []byte, v interface{}) error { return nil}
