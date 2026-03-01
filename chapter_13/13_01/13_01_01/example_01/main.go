870fc8eed998ef0e386f3a0f8eaf057393711e26
Asanbek09 <samudinua@gmail.com>
2026-03-01 16:59:06 +0600

    Три составляющие системы рефлексии

---
+++ chapter_13/13_01/13_01_01/example_01/main.go
@@ -0,0 +1,52 @@
+// Суммирование значений с переключением логики на основе типа
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type MyString string
+
+func (s MyString) ToFloat() (float64, error) {
+	fl, err := strconv.ParseFloat(string(s), 64)
+	if err != nil {
+		panic(err)
+	}
+
+	return fl, nil
+}
+
+func main() {
+	var a uint8 = 2
+	var b int = 37
+	var c string = "3.2"
+
+	res := sum(a, b, c)
+	fmt.Printf("Result: %f\n", res)
+}
+
+func sum(v ...interface{}) float64 {
+	var res float64 = 0.0
+	for _, val := range v {
+		switch val.(type) {
+		case int:
+			res += float64(val.(int))
+		case int64:
+			res += float64(val.(int64))
+		case uint8:
+			res += float64(val.(uint8))
+		case string:
+			a, err := strconv.ParseFloat(val.(string), 64)
+			if err != nil {
+				panic(err)
+			}
+
+			res += a
+		default:
+			fmt.Printf("Unsupported type %T. Ignoring \n", val)
+		}
+	}
+
+	return res
+}
